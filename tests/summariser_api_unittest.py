18cc96c4c17d970f1ce6b36dd54377430bd6dbba
ugmSorcero <ugm2@alu.ua.es>
2021-01-06 11:16:54 +0000

    Updates api unittest a little

--- tests/summariser_api_unittest.py
+++ tests/summariser_api_unittest.py
@@ -8,3 +8,26 @@
 Unit tests for summary API
 '''
 
+from fastapi.testclient import TestClient
+from summariser.api.server import app
+
+import unittest
+
+class SummariserAPITestCase(unittest.TestCase):
+    '''
+    Class for running unit tests on the Summariser API
+    '''
+    @classmethod
+    def setUpClass(cls):
+        '''
+        Set up all tests
+        '''
+        super(SummariserAPITestCase, cls).setUpClass()
+    
+    def setUp(self):
+        '''
+        Set up for each unit test
+        '''
+        self.client = TestClient(app)
+
+    
